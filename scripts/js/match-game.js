0fa821467a4542bdaba18ce2cdf2433d2867dc2c
Gunasai Garapati <garapatigunasai@gmail.com>
2017-11-03 16:12:54 -0400

    javascript to generate random values and cards.

--- scripts/js/match-game.js
+++ scripts/js/match-game.js
@@ -4,13 +4,31 @@ var MatchGame = {};
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
-
+$(document).ready(function() {
+	var $game = $('#game');
+	var values = MatchGame.generateCardValues();
+	MatchGame.renderCards(values, $game);
+});
 /*
   Generates and returns an array of matching card values.
  */
 
 MatchGame.generateCardValues = function () {
+	var arr = [];
+	for (var i = 1; i <= 8; i++) {
+		arr.push(i);
+		arr.push(i);
+	}
 
+	var cardValues = [];
+
+	while (arr.length > 0) {
+		var random = Math.floor(Math.random() * arr.length);
+		var randomValue = arr.splice(random, 1)[0];
+		cardValues.push(randomValue);  
+	}
+
+	return cardValues;
 };
 
 /*
@@ -19,7 +37,28 @@ MatchGame.generateCardValues = function () {
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
+	var colors = [
+		'hsl(25, 85%, 65%)',
+		'hsl(55, 85%, 65%)',
+		'hsl(90, 85%, 65%)',
+		'hsl(160, 85%, 65%)',
+		'hsl(220, 85%, 65%)',
+		'hsl(265, 85%, 65%)',
+		'hsl(310, 85%, 65%)',
+		'hsl(360, 85%, 65%)'
+	];
 
+	$game.empty();
+	for (var i = 0; i < cardValues.length; i++) {
+		var value = cardValues[i];
+		var color = colors[value - 1];
+		var $cardElement = $('<div class="card col-md-3 col-xs-3 col-lg-3"></div>');
+		$cardElement.data('value', cardValues[i]);
+		$cardElement.data('isFlipped', false);
+		$cardElement.data('color', color);
+
+		$game.append($cardElement);
+	}
 };
 
 /*
